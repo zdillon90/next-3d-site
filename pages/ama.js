c25b9ce8f65061747693750a03ee3b00d16c38b9
Zach Dillon <zdillon90@gmail.com>
2022-05-01 15:37:45 -0400

    ama format and added swr

--- pages/ama.js
+++ pages/ama.js
@@ -1,3 +1,4 @@
+import React, { useState } from 'react'
 import {
     FormControl,
     FormLabel,
@@ -12,33 +13,57 @@ import {
     Flex,
     Textarea,
     Divider,
+    Button,
   } from '@chakra-ui/react'
 
 export default function Ama() {
+    const [question, setQuestion] = useState('')
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+
     return (
         <Center>
             <Box w='50vh' m='20'>
                 <Heading p='2'>Ask Me Anything</Heading>
                     <FormControl>
                         <FormLabel>Question</FormLabel>
-                        <Textarea placeholder='Feel free to ask your question here' />
+                        <Textarea id='question' placeholder='Feel free to ask your question here' />
+                        <FormLabel>Name</FormLabel>
+                        <Input id='name' type='email' />
                         <FormLabel htmlFor='email'>Email address</FormLabel>
                         <Input id='email' type='email' />
                         <FormHelperText>I'll never share your email.</FormHelperText>
+                        <Flex justify='flex-end'>
+                            <Button
+                                mt={4}
+                                bg='white'
+                                variant='outline'
+                                // isLoading={props.isSubmitting}
+                                type='submit'
+                            >
+                                Submit
+                            </Button>
+                        </Flex>
                     </FormControl>
                     <Center height='50px'>
                         <Divider orientation='horizontal' />
                     </Center>
                 <Heading p='2'>Questions</Heading>
                 <Box p="5"  borderWidth="2px">
+                    <Text
+                        fontSize="xl"
+                        fontWeight="bold"
+                    >
+                        Name
+                    </Text>
                     <Flex align="baseline" mt={2}>
                         <Badge colorScheme="pink">Q</Badge>
                         <Text
                             ml={2}
                             // textTransform="uppercase"
                             fontSize="md"
-                            fontWeight="bold"
-                            // color="pink.800"
+                            // fontWeight="bold"
+                            color="pink.800"
                         >
                             What do you call a fish wearing a bow tie?
                         </Text>
